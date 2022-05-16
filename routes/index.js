ad47146c2dda492b17a219d25b54d4381cc61922
Carlos <carloslorenzo312@gmail.com>
2022-05-16 15:01:54 +0200

    pages & code postal hecho

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const router = express.Router();
+const axios = require('axios').default;
+
+
+router.get("/", async (req, res) => {
+
+    res.render('index')
+}); 
+
+router.post("/", async (req, res) =>{
+    var postcodes = []
+    let url_postcodes = "http://127.0.0.1:8000/api/postcodes/listar"; 
+    let response = await axios.get(url_postcodes)
+    postcodes = response.data.postcode
+    
+    var valido = false; 
+    postcodes.forEach(element => {
+        if(element.code == req.body.codepost){
+            valido = true
+        }
+    });
+
+    var mensaje = "Al código postal que has introducido no podemos hacer entregas:("; 
+    if(valido == true){
+        mensaje = "A ese código postal sí podemos hacer entregas, disfrútalo 😉"; 
+    }
+
+    res.render('index', {mensaje})
+}); 
 
 
 
-
-module.exports = router;
+module.exports = router;    
