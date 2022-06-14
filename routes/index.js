7dad0ab51d8d75620cc59d61885b34e72f98d2d4
Carlos <carloslorenzo312@gmail.com>
2022-06-15 01:02:43 +0200

    mis pedidos hechos

--- routes/index.js
+++ routes/index.js
@@ -75,42 +75,44 @@ router.get("/user/mispedidos", async(req, res) => {
     try{
 
         if(req.session.user){
-
+            let user = req.session.user.firstName
             let url_pedido = `http://127.0.0.1:8000/api/pedidos/user/${req.session.user.id}`
             const pedidos = await axios.get(url_pedido)
             let pedidos_user = []
             pedidos_user = pedidos.data.pedido
 
            
-            const pedidos_productos_array = []
+            var pedidos_productos_array = []
+
+            Promise.all(pedidos_user.map(async item => {
+
+                // Then put your try catch here so that it only wraps around
+                // the results of the function you're awaiting...
+                let id_pedido = item.id
+                let productos_query = `http://127.0.0.1:8000/api/pedidoDetalles/listarpedido/${id_pedido}`
+                let response
+                try {
+                  response = await axios.get(productos_query)
         
-            const productos_each_pedido = async () =>{
-                try{
-
-                    pedidos_user.map( async(element) => {
-
-                        let id_pedido = element.id
-                        let productos_query = `http://127.0.0.1:8000/api/pedidoDetalles/listarpedido/${id_pedido}`
-                        await axios.get(productos_query).then((response) =>{
-                            console.log("Entra")
-                            pedidos_productos_array = response.data.pedido_detalle
-                            console.log("Array")
-                            console.log(pedidos_productos_array)
-                        })
-                        
-                    }) 
-                        
-                    
-
-                }catch(err){
-                    console.log(err)
+                } catch (err) {
+                  return err;
                 }
-            }
-
-            productos_each_pedido()
-
-
-            return res.render("pedidos_user", {pedidos_user, pedidos_productos_array})
+              
+                // Anything else you want to do with the response...
+                return response.data.pedido_detalle
+              
+              })).then(results => {
+                // All the resolved promises returned from the map function.
+                pedidos_productos_array = results
+                if(req.session.cart){
+                    let totalItems = req.session.cart.totalItems
+                    
+                    return res.render('pedidos_user', {user, totalItems, pedidos_user, pedidos_productos_array})
+                }else{
+                    
+                    return res.render('pedidos_user', {user, pedidos_user, pedidos_productos_array})
+                }
+            })
             
 
            
